7fe75c6ecef9c04355b5ec9d833cacca92264218
Roaringmind22 <nandor1kovacs@gmail.com>
2021-05-17 18:23:32 +0200

    works suspicously (probably bad output)
    mixin works well

--- src/main/java/net/roaringmind/slimechunkf3/mixin/HudMixin.java
+++ src/main/java/net/roaringmind/slimechunkf3/mixin/HudMixin.java
@@ -1,23 +1,47 @@
 package net.roaringmind.slimechunkf3.mixin;
 
+import java.util.List;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
+import it.unimi.dsi.fastutil.longs.LongSet;
 import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.hud.DebugHud;
-import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.StructureWorldAccess;
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.WorldChunk;
+import net.minecraft.world.gen.ChunkRandom;
 
 @Mixin(DebugHud.class)
-public class HudMixin extends DrawableHelper{
+public class HudMixin extends DrawableHelper {
   public HudMixin() {
     super();
   }
 
-  @Inject(method = "renderLeftText", at = @At(value = "INVOKE_ASSIGN", target = "Ljava/util/List;add(Ljava/lang/Object;)Z", args = {
-    "log=true" }))
-  void setCustomDebugLine(MatrixStack matrices, final CallbackInfo info) {
-    System.out.println("mixin working");
+  @Inject(method = "getLeftText", at = @At(value = "INVOKE_ASSIGN", ordinal = 8, target = "Ljava/util/List;add(Ljava/lang/Object;)Z", args = {
+      "log=true" }), locals = LocalCapture.CAPTURE_FAILSOFT)
+  protected void setCustomDebugLine(CallbackInfoReturnable<List> cir, String string2, BlockPos blockPos, Entity entity,
+      Direction direction, String string7, World world, LongSet longSet, List list, WorldChunk worldChunk, int i, int j,
+      int k) {
+    boolean isSlimeChunk = false;
+
+    if (world instanceof StructureWorldAccess) {
+      StructureWorldAccess stra = (StructureWorldAccess) world;
+      boolean chunkRandom = ChunkRandom
+          .getSlimeRandom(worldChunk.getPos().x, worldChunk.getPos().z, stra.getSeed(), 987234911L).nextInt(10) == 0;
+
+      if (chunkRandom && world.random.nextInt(10) == 0) {
+        isSlimeChunk = true;
+      }
+    }
+
+    list.add(String.format("Slime Chunk: %s", String.valueOf(isSlimeChunk)));
   }
 }
