9a66d208bf37ec5f6129c4a8d460e45d7e7e7af1
NandorKovacs <nandor1kovacs@gmail.com>
2021-05-17 14:41:01 +0200

    can print mixin working

--- src/main/java/net/roaringmind/slimechunkf3/mixin/HudMixin.java
+++ src/main/java/net/roaringmind/slimechunkf3/mixin/HudMixin.java
@@ -5,13 +5,18 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.hud.DebugHud;
 import net.minecraft.client.util.math.MatrixStack;
 
 @Mixin(DebugHud.class)
-public class HudMixin {
-  
-  @Inject(method = "getlefttext()L", at = @At(value = "INVOKE_ASSIGN", ordinal = 8, target = "Lnet/minecraft/client/gui/hud/DebugHud;add(Ljava/lang/String;)Z"))
+public class HudMixin extends DrawableHelper{
+  public HudMixin() {
+    super();
+  }
+
+  @Inject(method = "renderLeftText", at = @At(value = "INVOKE_ASSIGN", target = "Ljava/util/List;add(Ljava/lang/Object;)Z", args = {
+    "log=true" }))
   void setCustomDebugLine(MatrixStack matrices, final CallbackInfo info) {
     System.out.println("mixin working");
   }
