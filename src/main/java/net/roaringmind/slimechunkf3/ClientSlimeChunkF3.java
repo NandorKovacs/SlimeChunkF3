3298d2dd2c723e347ac7e371252a3f9b3792b4fd
NandorKovacs <nandor1kovacs@gmail.com>
2021-06-29 09:05:51 +0200

    client command

--- src/main/java/net/roaringmind/slimechunkf3/ClientSlimeChunkF3.java
+++ src/main/java/net/roaringmind/slimechunkf3/ClientSlimeChunkF3.java
@@ -1,8 +1,16 @@
 package net.roaringmind.slimechunkf3;
 
+import static com.mojang.brigadier.arguments.LongArgumentType.getLong;
+import static com.mojang.brigadier.arguments.LongArgumentType.longArg;
+import static net.fabricmc.fabric.api.client.command.v1.ClientCommandManager.argument;
+import static net.fabricmc.fabric.api.client.command.v1.ClientCommandManager.literal;
+
+import static net.roaringmind.slimechunkf3.SlimeChunkF3.log;
+
 import org.apache.logging.log4j.Level;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.command.v1.ClientCommandManager;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 
 public class ClientSlimeChunkF3 implements ClientModInitializer {
@@ -10,16 +18,23 @@ public class ClientSlimeChunkF3 implements ClientModInitializer {
 
   @Override
   public void onInitializeClient() {
-    log("Client Initialize");
-    
+    log(Level.INFO, "Client Initialize");
+
     ClientPlayNetworking.registerGlobalReceiver(SlimeChunkF3.SEED_PACKET_ID, (client, handler, buf, sender) -> {
-      log("packet recieved");
-      
+      log(Level.INFO, "packet recieved");
+
       seed = buf.readLong();
     });
-  }
 
-  private void log(String message) {
-    SlimeChunkF3.log(Level.INFO, message);
+    ClientCommandManager.DISPATCHER
+        .register(
+          literal("slimechunkf3-seed")
+            .then(argument("seed", longArg())
+              .executes(ctx -> {
+                seed = getLong(ctx, "seed");
+                return 0;
+              })
+            )
+          );
   }
 }
--- src/main/java/net/roaringmind/slimechunkf3/SlimeChunkF3.java
+++ src/main/java/net/roaringmind/slimechunkf3/SlimeChunkF3.java
@@ -5,6 +5,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.lookup.v1.item.ItemApiLookup;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.Identifier;
 
 public class SlimeChunkF3 implements ModInitializer {
@@ -24,5 +26,4 @@ public class SlimeChunkF3 implements ModInitializer {
   public static void log(Level level, String message) {
     LOGGER.log(level, String.format("[%s] %s", MOD_NAME, message));
   }
-
 }
