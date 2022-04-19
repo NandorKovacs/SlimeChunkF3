5d4aab20b0dfaf6c26aaaa3dd17afda55527170e
NandorKovacs <nandor1kovacs@gmail.com>
2022-04-19 17:00:10 +0200

    updated to 1.18

--- src/main/java/net/roaringmind/slimechunkf3/mixin/HudMixin.java
+++ src/main/java/net/roaringmind/slimechunkf3/mixin/HudMixin.java
@@ -12,6 +12,8 @@ import it.unimi.dsi.fastutil.longs.LongSet;
 import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.hud.DebugHud;
 import net.minecraft.entity.Entity;
+import net.minecraft.network.ClientConnection;
+import net.minecraft.server.integrated.IntegratedServer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.math.Direction;
@@ -27,11 +29,8 @@ public class HudMixin extends DrawableHelper {
     super();
   }
 
-  @Inject(method = "getLeftText", at = @At(value = "INVOKE_ASSIGN", ordinal = 8, target = "Ljava/util/List;add(Ljava/lang/Object;)Z", args = {
-      "log=true" }), locals = LocalCapture.CAPTURE_FAILSOFT)
-  protected void setCustomDebugLine(CallbackInfoReturnable<List> cir, String string2, BlockPos blockPos, Entity entity,
-      Direction direction, String string7, World world, LongSet longSet, List list, WorldChunk worldChunk, int i, int j,
-      int k) {
+  @Inject(method = "getLeftText", at = @At(value = "INVOKE_ASSIGN", ordinal = 8, target = "Ljava/util/List;add(Ljava/lang/Object;)Z"), locals = LocalCapture.CAPTURE_FAILHARD)
+  protected void setCustomDebugLine(CallbackInfoReturnable<List> cir, String string, IntegratedServer integratedServer, ClientConnection clientConnection, float f, float g, BlockPos blockPos, Entity entity, Direction direction, String string2, ChunkPos chunkPos, World world, LongSet longSet, List list, String string3, WorldChunk worldChunk, int i, int j, int k) {
 
     Long seed = null;
 
@@ -44,8 +43,8 @@ public class HudMixin extends DrawableHelper {
     String message = "cant access seed";
 
     if (seed != null) {
-      ChunkPos chunkPos = worldChunk.getPos();
-      message = String.valueOf(ChunkRandom.getSlimeRandom(chunkPos.x, chunkPos.z, seed, 987234911L).nextInt(10) == 0);
+      ChunkPos mychunkPos = worldChunk.getPos();
+      message = String.valueOf(ChunkRandom.getSlimeRandom(mychunkPos.x, mychunkPos.z, seed, 987234911L).nextInt(10) == 0);
     }
 
     list.add(String.format("Slime Chunk: %s", message));
